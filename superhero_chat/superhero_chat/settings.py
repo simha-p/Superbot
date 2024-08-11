6c59fe680a7e0f5581d2c7ff02803187bdc1b048
Lakshmi DivyaSimhaGoud S <107531005+simha-p@users.noreply.github.com>
2024-08-12 00:16:58 +0530

    Update settings.py

--- superhero_chat/superhero_chat/settings.py
+++ superhero_chat/superhero_chat/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY','django-insecure-9=#yw^2^^)ig*xr$6gqgxj4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition

0974896c6e745970a295b227bb54f9baa4114028
Lakshmi DivyaSimhaGoud S <107531005+simha-p@users.noreply.github.com>
2024-08-12 00:32:43 +0530

    Update settings.py

--- superhero_chat/superhero_chat/settings.py
+++ superhero_chat/superhero_chat/settings.py
@@ -78,12 +78,6 @@ WSGI_APPLICATION = 'superhero_chat.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 DATABASES['default']=dj_database_url.config()
 
 # Password validation
